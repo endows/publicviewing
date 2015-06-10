cdcf3dc333776489533d068a22338fe221493eb8
Your Name <you@example.com>
2015-06-10 19:56:42 +0900

    remove autopublich

--- publicviewing.js
+++ publicviewing.js
@@ -1,4 +1,9 @@
-Users = new Meteor.Collection('users')
+Users = Meteor.users
+Users.allow({
+  update:function(){
+    return true
+  }
+})
 Channels = new Meteor.Collection('channels',{
   transform:function(doc){
     doc.visitors = Users.find({visiting_channel_id:doc._id}).fetch()
@@ -17,6 +22,7 @@ Router.route('/', function () {
 
 Router.route('/:_id', function () {
   Session.set('channel_id',this.params._id)
+
   Template.channel.helpers({
     channel:function(){
       return Channels.findOne(Session.get('channel_id'))
@@ -24,3 +30,7 @@ Router.route('/:_id', function () {
   })
   this.render('channel');
 });
+
+if(Meteor.isClient){
+
+}
